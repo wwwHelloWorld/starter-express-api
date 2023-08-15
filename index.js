7d6391d1e91b2bd0c40145e5f14cf63681b30122
Hello World <83601958+wwwHelloWorld@users.noreply.github.com>
2023-08-15 21:23:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,44 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
+import express, { json } from 'express';
+import cors from 'cors';
+const app = express();
+
+app.use(json()); // Middleware для парсинга JSON-данных
+app.use(cors()); // Enable CORS for all routes
+
+// Пример начального массива данных
+// eslint-disable-next-line max-len
+let data = {
+  gameData: [
+    'empty',
+    'empty',
+    'empty',
+    'empty',
+    'empty',
+    'empty',
+    'empty',
+    'empty',
+    'empty',
+  ],
+  isActiveGame: true,
+  activeSing: null,
+  isReset: false,
+  isGameOver: false,
+};
+
+// const gameOver = () => {
+//   return data.gameData.every((el) => el !== 'empty');
+// };
+
+// Роут для получения текущего состояния массива
+app.get('/data', (req, res) => {
+  res.json(data);
+});
+
+// Роут для обновления массива
+app.post('/update', (req, res) => {
+  const newData = req.body; // Получение новых данных из тела запроса
+  data = newData; // Обновление массива данных
+  console.log(data);
+  res.json(newData);
+});
 app.listen(process.env.PORT || 3000)
